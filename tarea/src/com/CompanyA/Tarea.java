e3b8476b1dd4d10ef49e4bd4d21a21834122fb27
Skeletoxs <110582953+Skeletoxs@users.noreply.github.com>
2023-06-18 23:19:10 -0400

    Tarea de JAVA Basico
    Aquí les envió la primera tarea de JAVA Básico

---
+++ tarea/src/com/CompanyA/Tarea.java
@@ -0,0 +1,40 @@
+package com.CompanyA;
+
+public class Tarea {
+
+    public static void main(String[] args) {
+        // Datos
+        //Numero enteros
+        byte  numero1 = 3;
+        short numero2 = 4;
+        int   numero3 = 15;
+        long  numero4 = 1000;
+
+        //Numero decimales
+        float  numero5 = 12.6f;
+        double numero6 = 15.60;
+
+        //Caracteres
+        char   letra = 'a';
+        String nombre = "Abraham";
+
+        //Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //Imprimir por pantalla
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
+        System.out.println(letra);
+        System.out.println(nombre);
+        System.out.println(verdadero);
+        System.out.println(falso);
+
+
+
+    }
+}
