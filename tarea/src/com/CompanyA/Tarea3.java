8a46420cf0d8768879ac3c2b89d71f98c0e70d79
Skeletoxs <110582953+Skeletoxs@users.noreply.github.com>
2023-06-21 15:14:14 -0400

    Tarea numero 3
    Aquí les envió la tarea numero 3 que, consiste en concatenar texto

---
+++ tarea/src/com/CompanyA/Tarea3.java
@@ -0,0 +1,13 @@
+package com.CompanyA;
+
+public class Tarea3 {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Laura", "Abraham", "Iliana", "Isaac", "Saul"};
+
+        for (String nombre : nombres) {
+            System.out.print(nombre + " ");
+        }
+    }
+}
